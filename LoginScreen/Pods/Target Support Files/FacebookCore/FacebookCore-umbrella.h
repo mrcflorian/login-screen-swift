8c0a9bdc538a9ab40f239c2b4f6955ea4c6e6fce
Florian Marcu <florian.mrc@gmail.com>
2017-01-16 20:25:09 -0800

    Removed unused files

--- LoginScreen/Pods/Target Support Files/FacebookCore/FacebookCore-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double FacebookCoreVersionNumber;
-FOUNDATION_EXPORT const unsigned char FacebookCoreVersionString[];
-
--- LoginScreen/Pods/Target Support Files/FacebookLogin/FacebookLogin-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double FacebookLoginVersionNumber;
-FOUNDATION_EXPORT const unsigned char FacebookLoginVersionString[];
-
